ded40c6534886f8f8fcbcd2c6d0884ff45bfd0ab
Scott Cooper <scttcper@gmail.com>
2018-06-18 12:49:12 -0700

    docs: remove version from title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
-
-import json from '../lib/package.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(title: Title) {
-    if (json) {
-      title.setTitle(`ngx-headroom ${json.version}`);
-    }
-  }
   handlePin() {
     console.log('pinned');
   }
