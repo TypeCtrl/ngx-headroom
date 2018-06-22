4df14443a01f48edcf6f82b1ae068250a001fb72
Scott Cooper <scttcper@gmail.com>
2018-06-22 16:19:54 -0700

    chore: mock github button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,25 @@
 import { async, TestBed } from '@angular/core/testing';
 
 import { NtkmeButtonModule } from '@ctrl/ngx-github-buttons';
+import { ButtonService } from '@ctrl/ngx-github-buttons';
+import { of as ObservableOf } from 'rxjs';
 
 import { HeadroomModule } from '../lib/public_api';
 import { AppFooterComponent } from './app-footer/app-footer.component';
 import { AppComponent } from './app.component';
+
+class FakeButtonService {
+  repo(user: string, repo: string) {
+    return ObservableOf({ stargazers_count: 0 });
+  }
+}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent, AppFooterComponent],
       imports: [NtkmeButtonModule, HeadroomModule],
+      providers: [{ provide: ButtonService, useClass: FakeButtonService }],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
