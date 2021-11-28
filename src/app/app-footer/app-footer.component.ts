2eebb3c9742037ce4dd1001b977586a2f04fa06e
Scott Cooper <scttcper@gmail.com>
2021-11-28 14:06:37 -0800

    feat: upgrade to angular 13 (#241)
    BREAKING CHANGE: requires angular > 12
    
    This reverts commit 31ef6ebf18dc6d2a51adb391dd90c3a170502f50.

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -3,24 +3,25 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template: `
-  <footer class="footer mb-4 mt-5">
-    Angular {{ version }}
-    <br>
-    Released under the
-    <a href="https://github.com/typectrl/ngx-headroom/blob/master/LICENSE">MIT</a> license
-    -
-    <a href="https://github.com/typectrl/ngx-headroom">View Source</a>
-  </footer>
+    <footer class="footer mb-4 mt-5">
+      Angular {{ version }}
+      <br />
+      Released under the
+      <a href="https://github.com/typectrl/ngx-headroom/blob/master/LICENSE">MIT</a> license -
+      <a href="https://github.com/typectrl/ngx-headroom">View Source</a>
+    </footer>
   `,
-  styles: [`
-  .footer {
-    line-height: 2;
-    text-align: center;
-    font-size: 69%;
-    color: #999;
-    font-family: var(--font-family-monospace);
-  }
-  `],
+  styles: [
+    `
+      .footer {
+        line-height: 2;
+        text-align: center;
+        font-size: 69%;
+        color: #999;
+        font-family: var(--font-family-monospace);
+      }
+    `,
+  ],
 })
 export class AppFooterComponent {
   version = VERSION.full;
